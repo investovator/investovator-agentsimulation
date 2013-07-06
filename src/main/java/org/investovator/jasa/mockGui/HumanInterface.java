e26854d53ec64f47dbe4ec063f1d3a66e1ab2d41
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-07-06 18:52:32 +0530

    Added a form so that human players can interact with the simulation

---
+++ src/main/java/org/investovator/jasa/mockGui/HumanInterface.java
@@ -0,0 +1,11 @@
+package org.investovator.jasa.mockGui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ishan
+ * Date: 7/6/13
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HumanInterface {
+}
