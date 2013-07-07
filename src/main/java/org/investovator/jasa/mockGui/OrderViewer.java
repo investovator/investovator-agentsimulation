51b89ff038b71cd39e37bbeb13572f0d40c1990e
Amila Surendra <amilasurendra@gmail.com>
2013-07-07 08:33:16 +0530

    added new form to temporary show placed orders

---
+++ src/main/java/org/investovator/jasa/mockGui/OrderViewer.java
@@ -0,0 +1,11 @@
+package org.investovator.jasa.mockGui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amila
+ * Date: 7/7/13
+ * Time: 8:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrderViewer {
+}
