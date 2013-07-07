2f1a12abc3a70058f764f57a428198c0a4fca9fa
Amila Surendra <amilasurendra@gmail.com>
2013-07-07 09:05:08 +0530

    Added adapter to try to unify multiple orderbooks

---
+++ src/main/java/net/sourceforge/jasa/market/MultiassetOrderbookAdapter.java
@@ -0,0 +1,11 @@
+package net.sourceforge.jasa.market;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amila
+ * Date: 7/7/13
+ * Time: 8:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiassetOrderbookAdapter {
+}
