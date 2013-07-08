1dc9a5f9345479b86ede1ef131dfd4033b931376
Amila Surendra <amilasurendra@gmail.com>
2013-07-08 10:26:42 +0530

    Added method to externally append log line (naive)

--- src/main/java/org/investovator/jasa/mockGui/OrderViewer.java
+++ src/main/java/org/investovator/jasa/mockGui/OrderViewer.java
@@ -1,5 +1,8 @@
 package org.investovator.jasa.mockGui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: amila
@@ -7,5 +10,24 @@ package org.investovator.jasa.mockGui;
  * Time: 8:32 AM
  * To change this template use File | Settings | File Templates.
  */
-public class OrderViewer {
+public class OrderViewer extends JFrame {
+
+    public JPanel rootPane;
+    private JTextArea textArea1;
+
+
+    public void printLine(final String line){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                textArea1.append(line);
+            }
+        });
+
+    }
+
+
+
+
 }
