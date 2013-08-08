4fd1674be8f572349397cfbab18ec241d011c954
Amila Surendra <amilasurendra@gmail.com>
2013-08-08 12:36:08 +0530

    Added Exchange class and implemented placeOrder method.
    Issues : Have to add all the methods.

---
+++ src/main/java/org/investovator/exchange/Exchange.java
@@ -0,0 +1,48 @@
+package org.investovator.exchange;
+
+import net.sourceforge.jasa.market.AuctionException;
+import net.sourceforge.jasa.market.Market;
+import net.sourceforge.jasa.market.MarketSimulation;
+import net.sourceforge.jasa.market.Order;
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Amila Surendra
+ * @version $Revision
+ */
+public class Exchange {
+
+
+    static Logger logger = Logger.getLogger(Exchange.class);
+
+    private HashMap<String, MarketSimulation> stocks = new HashMap<String, MarketSimulation>();
+
+    public void setStocks(List<MarketSimulation> stocks){
+
+        Iterator<MarketSimulation> iterator = stocks.iterator();
+
+        while (iterator.hasNext()) {
+            MarketSimulation next =  iterator.next();
+            this.stocks.put(next.getStockID() ,next);
+        }
+
+    }
+
+    public void placeOrder(String stockID, Order order){
+
+        MarketSimulation simulation = stocks.get(stockID);
+
+        try {
+            simulation.placeOrder(order);
+        } catch (AuctionException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+}
