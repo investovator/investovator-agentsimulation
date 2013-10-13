bf8d01a415e17e5553bd6ebe7e171089566274d7
Amila Surendra <amilasurendra@gmail.com>
2013-10-13 21:29:24 +0530

    Fixed bug in thread start - startSimulation

--- src/main/java/org/investovator/jasa/api/JASAFacade.java
+++ src/main/java/org/investovator/jasa/api/JASAFacade.java
@@ -57,7 +57,7 @@ public class JASAFacade implements MarketFacade {
 
     @Override
     public boolean startSimulation() {
-        new Thread(manager).run();
+        new Thread(manager).start();
         return true;
     }
 
