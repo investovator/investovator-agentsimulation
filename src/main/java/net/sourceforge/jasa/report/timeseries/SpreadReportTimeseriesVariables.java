30392ad5bbe75b590dbb45a1513a64ca192c1324
Rajith Siriwardana <rajithsiriwardana@gmail.com>
2013-11-22 18:36:14 +0530

    comments and license notice modified

--- src/main/java/net/sourceforge/jasa/report/timeseries/SpreadReportTimeseriesVariables.java
+++ src/main/java/net/sourceforge/jasa/report/timeseries/SpreadReportTimeseriesVariables.java
@@ -1,10 +1,25 @@
+/*
+ * investovator, Stock Market Gaming Framework
+ *     Copyright (C) 2013  investovator
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.sourceforge.jasa.report.timeseries;
 
-import net.sourceforge.jabm.event.RoundFinishedEvent;
 import net.sourceforge.jabm.event.SimEvent;
 import net.sourceforge.jasa.event.OrderPlacedEvent;
-import net.sourceforge.jasa.market.MarketQuote;
-import net.sourceforge.jasa.market.MarketSimulation;
 import net.sourceforge.jasa.market.Order;
 import net.sourceforge.jasa.market.auctioneer.ContinuousDoubleAuctioneer;
 
